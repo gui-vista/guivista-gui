79e65a43ab409d309f986408845476082d9943af
Nick Apperley <napperley@gmail.com>
2019-12-16 18:24:22 +1300

    Added Kotlin Multiplatform Gradle plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,22 @@
 group = "org.guivista"
 version = "0.1-SNAPSHOT"
+
+plugins {
+    kotlin("multiplatform") version "1.3.61"
+    id("maven-publish")
+}
+
+repositories {
+    jcenter()
+    mavenCentral()
+}
+
+kotlin {
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+            }
+        }
+    }
+}
