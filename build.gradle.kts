b388ed4044703c77c18e513bbaaedbfcdb60e982
Nick Apperley <napperley@gmail.com>
2019-12-26 13:53:36 +1300

    Added linuxArm32Main source set.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,18 +18,24 @@ kotlin {
                 val userIncludeDir = "/usr/include"
                 includeDirs(
                     "$userIncludeDir/atk-1.0",
-                    "$userIncludeDir/gdk-pixbuf-2.0",
-                    "$userIncludeDir/cairo",
-                    "$userIncludeDir/pango-1.0",
-                    "$userIncludeDir/gtk-3.0",
-                    "$userIncludeDir/glib-2.0",
-                    "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+                        "$userIncludeDir/gdk-pixbuf-2.0",
+                        "$userIncludeDir/cairo",
+                        "$userIncludeDir/pango-1.0",
+                        "$userIncludeDir/gtk-3.0",
+                        "$userIncludeDir/glib-2.0",
+                        "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
                 )
             }
         }
     }
+    linuxArm32Hfp("linuxArm32")
+
     sourceSets {
-        @Suppress("UNUSED_VARIABLE") val linuxX64Main by getting {
+        val linuxX64Main by getting {
+            languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
+        }
+        @Suppress("UNUSED_VARIABLE") val linuxArm32Main by getting {
+            dependsOn(linuxX64Main)
             languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
         }
     }
