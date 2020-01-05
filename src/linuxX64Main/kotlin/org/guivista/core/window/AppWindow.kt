f520b31e2928e4aa1571b00716da1138d2af30f9
Nick Apperley <napperley@gmail.com>
2020-01-06 12:35:43 +1300

    Added WindowBase interface.

--- src/linuxX64Main/kotlin/org/guivista/core/window/AppWindow.kt
+++ src/linuxX64Main/kotlin/org/guivista/core/window/AppWindow.kt
@@ -2,23 +2,28 @@ package org.guivista.core.window
 
 import gtk3.GtkApplicationWindow
 import gtk3.GtkWidget
+import gtk3.GtkWindow
 import gtk3.gtk_application_window_new
 import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.reinterpret
 import org.guivista.core.Application
 
-open class AppWindow(private val app: Application) : Window() {
+open class AppWindow(private val app: Application) : WindowBase {
     private var _gtkWidgetPtr: CPointer<GtkWidget>? = null
     override val gtkWidgetPtr: CPointer<GtkWidget>?
         get() = _gtkWidgetPtr?.reinterpret()
     val gtkAppWinPtr: CPointer<GtkApplicationWindow>?
-        get() = gtkWidgetPtr?.reinterpret()
+        get() = _gtkWidgetPtr?.reinterpret()
+    override val gtkWindowPtr: CPointer<GtkWindow>? by lazy {
+        // Have to manually specify the type for the reinterpret extension function for some strange reason.
+        _gtkWidgetPtr?.reinterpret<GtkWindow>()
+    }
 
-    override fun createUi(init: Window.() -> Unit) {
+    override fun createUi(init: WindowBase.() -> Unit) {
         _gtkWidgetPtr = gtk_application_window_new(app.gtkAppPtr)
         this.init()
         val mainLayout = createMainLayout()
-        if (mainLayout != null) addChild(mainLayout)
+        if (mainLayout != null) this += mainLayout
         if (visible) {
             showAll()
             resetFocus()
