87d11c91be3fb85db0c1c8dc797c72a98fd79dc5
Nick Apperley <napperley@gmail.com>
2020-04-24 15:59:49 +1200

    Updated Kotlin to 1.3.72.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ group = "org.guivista"
 version = "0.1-SNAPSHOT"
 
 plugins {
-    kotlin("multiplatform") version "1.3.61"
+    kotlin("multiplatform") version "1.3.72"
     id("maven-publish")
     id("org.jetbrains.dokka") version "0.10.0"
 }
@@ -16,37 +16,43 @@ repositories {
 }
 
 kotlin {
+    val guiVistaIoVer = "0.1-SNAPSHOT"
     linuxX64("linuxX64") {
         compilations.getByName("main") {
             cinterops.create("gtk3") {
                 val userIncludeDir = "/usr/include"
                 includeDirs(
                     "$userIncludeDir/atk-1.0",
-                        "$userIncludeDir/gdk-pixbuf-2.0",
-                        "$userIncludeDir/cairo",
-                        "$userIncludeDir/pango-1.0",
-                        "$userIncludeDir/gtk-3.0",
-                        "$userIncludeDir/glib-2.0",
-                        "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+                    "$userIncludeDir/gdk-pixbuf-2.0",
+                    "$userIncludeDir/cairo",
+                    "$userIncludeDir/pango-1.0",
+                    "$userIncludeDir/gtk-3.0",
+                    "$userIncludeDir/glib-2.0",
+                    "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
                 )
             }
             dependencies {
-                val guiVistaIoVer = "0.1-SNAPSHOT"
-                implementation("org.guivista:guivista-io:$guiVistaIoVer")
+                implementation("org.guivista:guivista-io-linuxx64:$guiVistaIoVer")
             }
         }
     }
     linuxArm32Hfp("linuxArm32")
 
     sourceSets {
+        @Suppress("UNUSED_VARIABLE")
         val linuxX64Main by getting {
             languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
         }
-        @Suppress("UNUSED_VARIABLE") val linuxArm32Main by getting {
-            dependsOn(linuxX64Main)
+
+        @Suppress("UNUSED_VARIABLE")
+        val linuxArm32Main by getting {
             languageSettings.useExperimentalAnnotation("kotlin.ExperimentalUnsignedTypes")
         }
     }
+}
+
+tasks.create("createLinuxLibraries") {
+    dependsOn("linuxX64MainKlibrary", "linuxArm32MainKlibrary")
 }
 
 tasks.getByName("dokka", DokkaTask::class) {
