d2515d773e4863a6ed98ea299da1dc7011d05c2e
Nick Apperley <napperley@gmail.com>
2021-01-15 10:23:51 +1300

    Improved tool_button.kt files.

--- src/linuxX64Main/kotlin/org/guiVista/gui/widget/tool/item/tool_button.kt
+++ src/linuxX64Main/kotlin/org/guiVista/gui/widget/tool/item/tool_button.kt
@@ -3,18 +3,22 @@ package org.guiVista.gui.widget.tool.item
 import gtk3.*
 import kotlinx.cinterop.CPointer
 import kotlinx.cinterop.reinterpret
+import org.guiVista.gui.widget.WidgetBase
 
-public actual class ToolButton(toolButtonPtr: CPointer<GtkToolButton>? = null, iconWidget: CPointer<GtkWidget>?,
-                               label: String) : ToolButtonBase {
+public actual class ToolButton(
+    toolButtonPtr: CPointer<GtkToolButton>? = null,
+    iconWidget: WidgetBase?,
+    label: String
+) : ToolButtonBase {
     override val gtkToolItemPtr: CPointer<GtkToolItem>? =
-        toolButtonPtr?.reinterpret() ?: gtk_tool_button_new(iconWidget, label)
+        toolButtonPtr?.reinterpret() ?: gtk_tool_button_new(iconWidget?.gtkWidgetPtr, label)
     override val gtkToolButtonPtr: CPointer<GtkToolButton>?
         get() = gtkToolItemPtr?.reinterpret()
 }
 
 public fun toolButtonWidget(
     toolButtonPtr: CPointer<GtkToolButton>? = null,
-    iconWidget: CPointer<GtkWidget>?,
+    iconWidget: WidgetBase?,
     label: String,
     init: ToolButton.() -> Unit = {}
 ): ToolButton {
